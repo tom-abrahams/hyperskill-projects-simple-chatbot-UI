8da18fff9d08349c67aa6fbdc349e87c0c27e5d8
Tom Abrahams <tom@at-it.be>
2024-05-17 21:19:53 +0200

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,4 @@
+//let user_name = prompt("Wat is jouw naam?");
+//console.log(`Dag ${user_name}`);
+
+// Next step: add event listeners for inputs when value changes: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
